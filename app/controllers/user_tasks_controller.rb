2da3c2bc4f5a933113a6193e506ed743d4674527
George Kettle <georgekettle.gk@gmail.com>
2021-04-14 15:06:03 +1000

    cleaned up code a little

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -39,10 +39,7 @@ class UserTasksController < ApplicationController
   end
 
   def change_user_position
-    date = @user_task.task.start_date
-    current_user.user_tasks_on_date(date).reverse.each do |user_task|
-      user_task.move_to_top
-    end
+    resort_user_tasks_on_date
     if params[:position] && @user_task.insert_at(params[:position].to_i)
       respond_to do |format|
         format.json { render json: @user_task }
@@ -64,6 +61,13 @@ class UserTasksController < ApplicationController
   end
 
   private
+
+  def resort_user_tasks_on_date
+    date = @user_task.task.start_date
+    current_user.user_tasks_on_date(date).reverse.each do |user_task|
+      user_task.move_to_top
+    end
+  end
 
   def set_profiles
     params[:user_task][:profile_id].map do |profile_id|
