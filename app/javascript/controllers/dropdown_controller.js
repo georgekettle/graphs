d2742d9af5b902a23e8c6c4c15cd059feb951105
George Kettle <georgekettle.gk@gmail.com>
2021-04-21 17:16:18 +1000

    styles moved to js, loading screen added, bucket filters added

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,13 +2,20 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "toggle", "container" ]
-  static classes = [ "open" ]
+  static classes = [ "open", "close" ]
 
   connect() {
-
+    // console.log(this.toggleTarget);
+    // console.log(this.containerTarget);
+    // console.log(this.openClass);
+    this.containerTarget.classList.add(this.closeClass);
   }
 
-  focusInput() {
-    this.inputTarget.focus()
+  toggleDropdown(e) {
+    e.preventDefault();
+    // console.log('toogleDropdown');
+    this.containerTarget.classList.add('animated');
+    this.containerTarget.classList.toggle(this.openClass);
+    this.containerTarget.classList.toggle(this.closeClass);
   }
 }
