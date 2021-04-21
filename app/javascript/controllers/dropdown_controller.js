0a2ca8b67c2780dd5dad52a9b57d747cf2d851fb
George Kettle <georgekettle.gk@gmail.com>
2021-04-21 17:23:00 +1000

    animation changed to not include scaling in fadeIn & fadeOut

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -5,15 +5,11 @@ export default class extends Controller {
   static classes = [ "open", "close" ]
 
   connect() {
-    // console.log(this.toggleTarget);
-    // console.log(this.containerTarget);
-    // console.log(this.openClass);
     this.containerTarget.classList.add(this.closeClass);
   }
 
   toggleDropdown(e) {
     e.preventDefault();
-    // console.log('toogleDropdown');
     this.containerTarget.classList.add('animated');
     this.containerTarget.classList.toggle(this.openClass);
     this.containerTarget.classList.toggle(this.closeClass);
